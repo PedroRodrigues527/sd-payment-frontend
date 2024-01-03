4b89c64f232bc590fc35783f2b3b52ec580c7168
pedrotiago12345 <49659345+pedrotiago12345@users.noreply.github.com>
2024-01-03 21:08:00 +0000

    coisas

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -3,13 +3,10 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul style={{ display: "flex", gap: "30px" }}>
-          <li>
-            <Link to="/">Listagem de pagamentos</Link>
-          </li>
+        <ul className="menuNavegacao">
+          <li><Link to="/">Listagem de pagamentos</Link></li>
+          <li><Link to="/login">To Login</Link></li>
         </ul>
-      </nav>
 
       <Outlet />
     </>
